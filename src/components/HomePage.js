06f4489e331715ca036817fb78586601ca9e8817
gregorysl <gregorysl@gmail.com>
2017-03-12 22:00:52 +0100

    quick check for diff func

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router';
 
+function weeks_between(date1, date2) {
+	const ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
+	const date1_ms = date1.getTime();
+	const date2_ms = date2.getTime();
+	const difference_ms = Math.abs(date1_ms - date2_ms);
+	return Math.floor(difference_ms / ONE_WEEK);
+}
 const HomePage = () => {
+  const natka = new Date("2016-11-03T18:40:00");
+  const weeks = weeks_between(natka,new Date());
   return (
     <div>
-      <h1>React Slingshot</h1>
+      <h1>Weeks since Natka was born: {weeks}</h1>
 
-      <h2>Get Started</h2>
-      <ol>
-        <li>Review the <Link to="fuel-savings">demo app</Link></li>
-        <li>Remove the demo and start coding: npm run remove-demo</li>
-      </ol>
     </div>
   );
 };
