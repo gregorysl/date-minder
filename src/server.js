cd1d3581965977249231675e41939da72c7d9d41
gregorysl <gregorysl@gmail.com>
2017-03-28 17:07:23 +0200

    Add save method to temp db serv

--- src/server.js
+++ src/server.js
@@ -2,30 +2,32 @@ const db = function () {
     let localData = JSON.parse(localStorage.getItem("data"));
     return localData || { index: 0 };
 };
+const save = function (data) {
+    localStorage.setItem("data", JSON.stringify(data));
+};
 let server = {
     add(item) {
         const data = db();
         data.index++;
-        debugger;
         item.id = data.index;
         data[data.index] = item;
-        localStorage.setItem("data", JSON.stringify(data));
+        save(data);
         return data;
     },
     update(item) {
         const id = item.id;
         const data = db();
         data[id] = item;
-        localStorage.setItem("data", JSON.stringify(data));
+        save(data);
         return data;
     },
     delete(id) {
         const data = db();
         delete data[id];
-        localStorage.setItem("data", JSON.stringify(data));
+        save(data);
         return data;
     },
-    getData(){
+    getData() {
         return db();
     }
 };
